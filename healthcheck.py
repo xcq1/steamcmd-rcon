c27cc73f390060ae76ca21e5c4c3ac449b6c35c9
Tobias Kuhn <mail@tobiaskuhn.de>
2017-07-08 12:43:57 +0200

    Fix python script errors and make unhealthy more verbose

--- healthcheck.py
+++ healthcheck.py
@@ -2,12 +2,12 @@ import sys
 import os
 import re
 sys.path.append(os.path.abspath("/rcon"))
-import SourceRcon as rcon
+import SourceRcon
 
 challenge = os.environ.get('RCON_HEALTH_COMMAND')
 check = os.environ.get('RCON_HEALTH_REGEXP')
 
-rcon = SourceRcon.SourceRcon(os.environ.get('RCON_HOST'), os.environ.get('RCON_PORT'), os.environ.get('RCON_PASSWORD'))
+rcon = SourceRcon.SourceRcon(os.environ.get('RCON_HOST'), int(os.environ.get('RCON_PORT')), os.environ.get('RCON_PASSWORD'))
 response = rcon.rcon(challenge)
 rcon.disconnect()
 
@@ -16,5 +16,8 @@ if len(check) > 0:
 	healthy = p.search(response) is not None
 else:
 	healthy = response is not None and len(response) > 0
+
+if not healthy:
+	print "Unhealthy response was '" + response + "'"
 
 sys.exit(0 if healthy else 1)
